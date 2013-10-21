416844bad82e245f140b7063f354516304b6d6fe
bryanation <reyes_bryan13@yahoo.com>
2013-10-21 20:13:30 +0800

    lasta lastsaf

--- index.php
+++ index.php
@@ -24,7 +24,6 @@
       <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
-
 // DB connection info
 $host = "tcp:i4kafcgsvr.database.windows.net,1433";
 $user = "bryan@i4kafcgsvr";
@@ -38,15 +37,13 @@ try {
 catch(Exception $e){
     die(var_dump($e));
 }
-
-
 if(!empty($_POST)) {
 try {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $date = date("Y-m-d");
     // Insert data
-    $sql_insert = "INSERT INTO registration (name, email, date) 
+    $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
                    VALUES (?,?,?)";
     $stmt = $conn->prepare($sql_insert);
     $stmt->bindValue(1, $name);
@@ -59,9 +56,7 @@ catch(Exception $e) {
 }
 echo "<h3>Your're registered!</h3>";
 }
-
-
-$sql_select = "SELECT * FROM registration";
+$sql_select = "SELECT * FROM registration_tbl";
 $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll(); 
 if(count($registrants) > 0) {
@@ -79,8 +74,6 @@ if(count($registrants) > 0) {
 } else {
     echo "<h3>No one is currently registered.</h3>";
 }
-
-
 ?>
 </body>
 </html>
